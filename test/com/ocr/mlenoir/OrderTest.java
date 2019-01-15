9d2632a5acd66669460e5c16b9d3ab11e33f1b9e
Maxime LENOIR <maxime.lenoir@aperam.com>
2019-01-15 15:15:53 +0100

    Add new tests in OrderTest

--- test/com/ocr/mlenoir/OrderTest.java
+++ test/com/ocr/mlenoir/OrderTest.java
@@ -8,7 +8,6 @@ import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 
 class OrderTest {
     private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
@@ -25,16 +24,37 @@ class OrderTest {
     }
 
     @Test
-    void Given_Nothing_When_DisplayAvailableMenu_Then_ShouldDisplayAvailableMenu() {
-        this.order.displayAvailableMenu();
+    void Given_Chicken_When_DisplaySelectedMenu_Then_DisplayChickenSentence() {
+        this.order.displaySelectedMenu(1);
 
-        assertFalse(outContent.toString().isEmpty());
+        assertEquals("Vous avez choisi le menu : Poulet\n", this.outContent.toString().replace("\r\n", "\n"));
     }
-    
+
     @Test
-    void Given_Vegetarian_When_AskForMenuSelected_Then_ShouldDisplayCorrectSentence() {
+    void Given_Beef_When_DisplaySelectedMenu_Then_DisplayBeefSentence() {
+        this.order.displaySelectedMenu(2);
+
+        assertEquals("Vous avez choisi le menu : Boeuf\n", this.outContent.toString().replace("\r\n", "\n"));
+    }
+
+    @Test
+    void Given_Vegetarian_When_DisplaySelectedMenu_Then_DisplayVegetarianSentence() {
         this.order.displaySelectedMenu(3);
 
-        assertEquals("Vous avez choisi le menu 3\n", outContent.toString().replace("\r\n", "\n"));
+        assertEquals("Vous avez choisi le menu : Végétarien\n", this.outContent.toString().replace("\r\n", "\n"));
+    }
+
+    @Test
+    void Given_TooBigValue_When_DisplaySelectedMenu_Then_DisplayErrorSentence() {
+        this.order.displaySelectedMenu(5);
+
+        assertEquals("Vous n'avez pas choisi de menu parmi les choix proposés\n", this.outContent.toString().replace("\r\n", "\n"));
+    }
+
+    @Test
+    void Given_NegativeValue_When_DisplaySelectedMenu_Then_DisplayErrorSentence() {
+        this.order.displaySelectedMenu(-6);
+
+        assertEquals("Vous n'avez pas choisi de menu parmi les choix proposés\n", this.outContent.toString().replace("\r\n", "\n"));
     }
 }
