ddc962d88d9d548f6840009d8804ffcf9c852809
Maxime LENOIR <maxime.lenoir@aperam.com>
2019-01-16 10:54:02 +0100

    Use runMenus method in Main

--- src/com/ocr/mlenoir/Main.java
+++ src/com/ocr/mlenoir/Main.java
@@ -3,6 +3,6 @@ package com.ocr.mlenoir;
 public class Main {
     public static void main(String[] args) {
         Order order = new Order();
-        order.runMenu();
+        order.runMenus();
     }
 }
