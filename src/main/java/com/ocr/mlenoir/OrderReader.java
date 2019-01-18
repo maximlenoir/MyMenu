9d71aab6f5eb3f193d0e1be4cab82a6983a55e3f
Maxime LENOIR <maxime.lenoir@aperam.com>
2019-01-18 10:44:54 +0100

    Use csv-commons library in OrderReader read method

--- src/main/java/com/ocr/mlenoir/OrderReader.java
+++ src/main/java/com/ocr/mlenoir/OrderReader.java
@@ -1,10 +1,12 @@
 package com.ocr.mlenoir;
 
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.List;
+import java.io.Reader;
 
 public class OrderReader {
     public static void main(String[] args) {
@@ -13,49 +15,41 @@ public class OrderReader {
     }
 
     private void read() {
-        Path orderFilePath = Paths.get("order.csv");
-        List<String> orderLines = null; // null mean no value by default
-
-        try {
-            orderLines = Files.readAllLines(orderFilePath);
-        } catch (IOException e) {
-            System.out.println("Oops! Impossible de lire le fichier des commandes.");
-        }
-
-        if (orderLines.size() < 2) {
-            System.out.println("Il n'y a pas de commandes dans le fichier.");
-
-            return;
-        }
-
         String[] menus = {"Menu Poulet", "Menu Boeuf", "Menu Végétarien"};
         String[] sides = {" avec des légumes frais", " avec des frites", " avec du riz"};
         String[] vegetarianSides = {" avec du riz", " sans riz"};
         String[] drinks = {" et avec de l'eau plate", " et avec de l'eau gazeuse", " et avec du soda"};
 
-        for (int i = 1; i < orderLines.size(); i++) {
-            String[] split = orderLines.get(i).split(",");
+        try {
+            Reader orderFileReader = new FileReader("order.csv");
+            Iterable<CSVRecord> records = CSVFormat.RFC4180.withFirstRecordAsHeader().parse(orderFileReader);
 
-            int selectedMenu = Integer.valueOf(split[0]);
-            int selectedSide = Integer.valueOf(split[1]);
-            int selectedDrink = Integer.valueOf(split[2]);
+            for (CSVRecord record : records) {
+                int selectedMenu = Integer.valueOf(record.get("menu"));
+                int selectedSide = Integer.valueOf(record.get("side"));
+                int selectedDrink = Integer.valueOf(record.get("drink"));
 
-            String orderLine = menus[selectedMenu - 1];
+                String orderLine = menus[selectedMenu - 1];
 
-            // Vegetarian menu
-            if (selectedMenu == 3) {
-                orderLine = orderLine.concat(vegetarianSides[selectedSide - 1]);
-            } else {
-                orderLine = orderLine.concat(sides[selectedSide - 1]);
+                // Vegetarian menu
+                if (selectedMenu == 3) {
+                    orderLine = orderLine.concat(vegetarianSides[selectedSide - 1]);
+                } else {
+                    orderLine = orderLine.concat(sides[selectedSide - 1]);
+                }
+
+                if (selectedDrink == -1) {
+                    orderLine = orderLine.concat(" et sans boisson");
+                } else {
+                    orderLine = orderLine.concat(drinks[selectedDrink - 1]);
+                }
+
+                System.out.println(orderLine);
             }
-
-            if (selectedDrink == -1) {
-                orderLine = orderLine.concat(" et sans boisson");
-            } else {
-                orderLine = orderLine.concat(drinks[selectedDrink - 1]);
-            }
-
-            System.out.println(orderLine);
+        } catch (FileNotFoundException e) {
+            System.out.println("Oops! Impossible de trouver le fichier de commandes 'order.csv'.");
+        } catch (IOException e) {
+            System.out.println("Oops! Une erreur est survenue durant la lecture du fichier de commandes 'order.csv'.");
         }
     }
 }
